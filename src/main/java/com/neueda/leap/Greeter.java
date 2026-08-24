48481ef127c36f7f25b4a1fb0ef82e1c9bc995ee
Sean Cheema <92892168+seankcheema@users.noreply.github.com>
2026-08-24 15:04:58 -0500

    feat: springboot init

--- src/main/java/com/neueda/leap/Greeter.java
+++ src/main/java/com/neueda/leap/Greeter.java
@@ -1,5 +1,8 @@
 package com.neueda.leap;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class Greeter {
     public String greet(String name) {
         return "Good day, " + name;
